4f0e7edeff62cb0e613c79a421a79e31fbf6c636
car23979 <125841463+car23979@users.noreply.github.com>
2025-03-10 12:25:00 -0600

    Subrutina Configurar_puertos
    Se crea la subrutina para configurar puertos

--- ProyectoNo1_Reloj_23979.asm
+++ ProyectoNo1_Reloj_23979.asm
@@ -188,6 +188,7 @@ CONFIGURAR_BOTONES:
     LDI     R16, 0x0F
     OUT     PORTC, R16
 
+CONFIGURAR_PUERTOS:
     // Configurar PORTB como salida para selección de displays
     LDI     R16, 0xFF		// 0b00001111 (PB0-PB3 como salidas)
     OUT     DDRB, R16
