10e18cc3c8b76468ce630f1eb839c9a555873603
car23979 <125841463+car23979@users.noreply.github.com>
2025-03-21 10:29:06 -0600

    EJECUTAR MODOS
    Se crearon las subrutinas para lograr ejecutar los modos entre estos que los incrementos y decrementos respecto cada digito, es decir mes, hora, minuto y dia

--- Reloj Proyecto1/main.asm
+++ Reloj Proyecto1/main.asm
@@ -548,6 +548,57 @@ ALARM_OFF:
 	CALL	TURN_ME_OFF
 	RET
 
+// -----------------------------------Subrutinas para ejecutar modos----------------------------------------------------
+INC_MIN: 
+	CALL	INC_DISP1
+	LDI		ACCION, 0x00
+	RET
+DEC_MIN: 
+	CALL	DEC_DISP1
+	LDI		ACCION, 0x00
+	RET
+INC_HOR: 
+	CALL	INC_DISP2
+	LDI		ACCION, 0x00
+	RET
+DEC_HOR: 
+	CALL	DEC_DISP2
+	LDI		ACCION, 0x00
+	RET
+
+INC_MES: 
+	CALL	INC_DISP_MES
+	LDI		ACCION, 0x00
+	RET
+DEC_MES: 
+	CALL	DEC_DISP_MES
+	LDI		ACCION, 0x00
+	RET
+INC_DIA: 
+	CALL	INC_DISP_DIA
+	LDI		ACCION, 0x00
+	RET
+DEC_DIA: 
+	CALL	DEC_DISP_DIA
+	LDI		ACCION, 0x00
+	RET
+
+INC_MIN_ALARM:
+	CALL	INC_DISP_MINAL
+	LDI		ACCION, 0x00
+	RET
+DEC_MIN_ALARM:
+	CALL	DEC_DISP_MINAL
+	LDI		ACCION, 0x00
+	RET
+INC_HOR_ALARM:
+	CALL	INC_DISP_HORAL
+	LDI		ACCION, 0x00
+	RET
+DEC_HOR_ALARM:
+	CALL	DEC_DISP_HORAL
+	LDI		ACCION, 0x00
+	RET
 	/*
 
 // MODIFICACIONES NUEVAS
@@ -585,262 +636,6 @@ BOTON_ISR:
 	BREQ	CONTINUAR_BOTONES	// Si buzzer_flag=0, continuar con botones
 	SBIC	R16, 3			// Si el boton 4 esta presionado
 	RJMP	APAGAR_BUZZER		// Ir a la subrutina de apagado
-
-CONTINUAR_BOTONES:
-	// Botón1 (PC0) cambio de modo
-	SBIC	R16, PC0
-	RJMP	CAMBIAR_MODO
-
-	// Botón2 (PC1) incrementar
-	SBIC	R16, PC1
-	RJMP	INCREMENTAR_VALOR
-
-	// Botón3 (PC2) decrementar
-	SBIC	R16, PC2
-	RJMP	DECREMENTAR_VALOR
-
-	// Botón4 (PC3) Confirmar
-	SBIC	R16, PC3
-	RJMP	CONFIRMAR
-
-APAGAR_BUZZER:
-	CBI		PORTD, 7		// Apagar buzzer en PD7
-	CLR		BUZZER_FLAG		// Indicar que el buzzer ya no esta sonando
-	RJMP	FIN_BOTON_ISR
-
-FIN_BOTON_ISR:
-	POP		R16
-	OUT		SREG, R16
-	POP		R16
-	RETI
-
-CONFIRMAR:
-	PUSH	R16
-	IN		R16, SREG
-	PUSH	R16
-
-	// Verificar modo de reloj
-	CPI		MODE, 1
-	BRNE	VERIFICAR_FECHA
-
-	// Si estamos en modo configuración de hora (mode=1)
-	LDS		R16, TEMP_HORA_ADDR
-	MOV		HORA, R16
-	LDS		R16, TEMP_MINUTO_ADDR
-	MOV		MINUTO, R16
-	RJMP	FIN_CONFIRMAR
-
-
-
-VERIFICAR_FECHA:
-	CPI		MODE, 2
-	BRNE	VERIFICAR_ALARMA
-
-	// Si estamos en modo de configuración de fecha (mode=2)
-	LDS		R16, TEMP_DIA_ADDR
-	MOV		DIA, R16
-	LDS		R16, TEMP_MES_ADDR
-	MOV		MES, R16
-	RJMP	FIN_CONFIRMAR
-
-VERIFICAR_ALARMA:
-	CPI		MODE, 3
-	BRNE	FIN_CONFIRMAR
-
-	// Si estamos en modo de configuración de alarma (mode=3)
-	LDS		R16, TEMP_HORA_ALARMA_ADDR
-	MOV		HORA_ALARMA, R16
-	LDS		R16, TEMP_MIN_ALARMA_ADDR
-	MOV		MIN_ALARMA, R16
-
-FIN_CONFIRMAR:
-	POP		R16
-	OUT		SREG, R16
-	POP		R16
-	RET
-
-CAMBIAR_MODO:
-	INC		MODE
-	CPI		MODE, 4		// Si el modo llega a 4, reiniciar a 0 	
-	BRNE	CONTINUAR_MODO
-	CLR		MODE
-
-CONTINUAR_MODO:
-	// Apagar todos los Leds de modo antes de encender el correcto
-	CBI		PORTC, 4	// Apagar LED de modo hora (A4)
-	CBI		PORTC, 5	// Apagar LED de modo fecha (A5)
-
-	// Encender los LEDs según el modo actual
-	CPI		MODE, 1
-	BRNE	MODO_FECHA
-	SBI		PORTC, 4	// Encender LED de modo hora
-	RJMP	FIN_CAMBIAR
-
-MODO_FECHA:
-	CPI		MODE, 2
-	BRNE	MODO_ALARMA
-	SBI		PORTC, 5	// Encender LED modo fecha
-	RJMP	FIN_CAMBIAR
-
-MODO_ALARMA:
-	CPI		MODE, 3
-	BRNE	FIN_CAMBIAR
-	SBI		PORTC, 4	// Encender ambos LEDs para Modo Alarma
-	SBI		PORTC, 5
-
-FIN_CAMBIAR:
-	RET
-
-INCREMENTAR_VALOR:
-	CPI		MODE, 1		// Si estamos en modo Hora
-	BREQ	INC_HORA	
-	CPI		MODE, 2		// Si estamos en modo Fecha
-	BREQ	INC_DIA
-	CPI		MODE, 3		// Si estamos en modo Alarma
-	BREQ	INC_HORA_ALARMA
-	RET
-
-INC_HORA:
-    INC     HORA
-    CPI     HORA, 24  // Si llega a 24, reiniciar 0
-    BRNE    FIN_INC
-    CLR		HORA
-
-FIN_INC:
-	RET
-
-INC_DIA:	
-	// Cuando llegue a 1, se va a reiniciar día maximo y va a ir al mes anterior mes
-	PUSH	R16
-	PUSH	R26
-	PUSH	R30
-	PUSH	R31
-
-	// Cambiar indice (1-12) a (0-11)
-	MOV		R26, MES
-	DEC		R26		// R26 va ser el indice de (0-11)
-	
-	// Cargar DIAS_MAX en Z
-	LDI		R30, LOW(DIAS_MAX)
-	LDI		R31, HIGH(DIAS_MAX)
-	
-	// Sumar al indice para acceder al valor correcto
-	ADD		R30, R26	// R30 apunta a DIAS_MAX
-	
-	// Cargar el valor maximo de días
-	LD		R16, Z
-
-	// Comparar día maximo con lo maximo permitido
-	CP		DIA, R16
-	BRLT	CONTINUAR_INC
-
-	// Si DIA es igual al maximo se reinicia 1 y avanza Mes
-	LDI		DIA, 1
-	INC		MES
-	CPI		MES, 13
-	BRNE	FIN_INC_DIA
-	LDI		MES, 1		// Si Mes es 13, reiniciar en 1 (Enero)
-	
-	RJMP	FIN_INC_DIA
-
-CONTINUAR_INC:
-	INC		DIA		// Incremento de día
-
-FIN_INC_DIA:
-	POP		R31
-	POP		R30
-	POP		R26
-	POP		R16
-	RET
-
-INC_HORA_ALARMA:
-	LDS		R16, TEMP_HORA_ALARMA_ADDR
-	INC		R16
-	CPI		R16, 24		// Si llega a 24, reiniciar 0
-	BRNE	FIN_INC_ALARMA
-	CLR		R16
-
-FIN_INC_ALARMA:
-	STS		TEMP_HORA_ALARMA_ADDR, R16
-	RET
-
-
-DECREMENTAR_VALOR:
-	CPI		MODE, 1		// Si estamos en modo Hora
-	BREQ	DEC_HORA	
-	CPI		MODE, 2		// Si estamos en modo Fecha
-	BREQ	DEC_DIA
-	CPI		MODE, 3		// Si estamos en modo Alarma
-	BREQ	DEC_HORA_ALARMA
-	RET
-
-DEC_HORA:
-	DEC		HORA
-	CPI		HORA, 23	// Si llega a 0, reiniciar 23
-	BRNE	FIN_DEC
-	CLR		HORA
-
-FIN_DEC:
-	RET
-
-DEC_DIA:	
-	PUSH	R16
-	PUSH	R26
-	PUSH	R30
-	PUSH	R31
-	
-	// Verificar si el día es mayor a 1
-	CPI		DIA, 1
-	BRNE	CONTINUAR_DEC
-
-	// Si el día es 1 retroceder al mes anterior
-	DEC		MES
-	CPI		MES, 0
-	BRNE	CONTINUAR_DEC2
-	LDI		MES, 12		// Si el mes era 1, va cambiar a diciembre (12)
-
-CONTINUAR_DEC2:
-	// Cambiar indice (1-12) a (0-11)
-	MOV		R26, MES
-	DEC		R26		// R26 va ser el indice de (0-11)
-	 
-	// Cargar DIAS_MAX en Z
-	LDI		R30, LOW(DIAS_MAX)
-	LDI		R31, HIGH(DIAS_MAX)
-	
-	// Sumar al indice para acceder al valor correcto
-	ADD		R30, R26	// R30 apunta a DIAS_MAX
-	
-	// Cargar el valor maximo de días
-	LD		R16, Z
-	MOV		DIA, R16	// Establecer día en el maximo del mes anterior
-	RJMP	FIN_DEC_DIA
-
-CONTINUAR_DEC:
-	DEC		DIA		// Decremento de días
-
-FIN_DEC_DIA:
-	POP		R31
-	POP		R30
-	POP		R26
-	POP		R16
-	RET
-
-DEC_HORA_ALARMA:
-	LDS		R16, TEMP_HORA_ALARMA_ADDR
-	CPI		R16, 0
-	BRNE	NORMAL_DEC_ALARMA
-	LDI		R16, 23		// Si está en 0, reiniciar a 23
-	RJMP	FIN_DEC_ALARMA
-
-NORMAL_DEC_ALARMA:
-	DEC		R16
-
-FIN_DEC_ALARMA:
-	STS		TEMP_HORA_ALARMA_ADDR, R16
-	RET
-
-
 
 
 TIMER0_ISR:
